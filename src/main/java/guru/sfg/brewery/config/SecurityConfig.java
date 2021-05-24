283f892f7cefa49126aedf061b76112e6911c6a9
fullstack-tahvildarii <coding.tahvildari@gmail.com>
2021-05-24 12:07:33 +0200

    security config matchers

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -1,20 +1,25 @@
 package guru.sfg.brewery.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+                http
                 .authorizeRequests(authorize -> {
-                    authorize.antMatchers("/","/webjars/**", "/login", "/resources/**").permitAll();
-                    authorize.antMatchers("/beers/find","/beer*" ).permitAll();
-                })
+                    authorize
+                            .antMatchers("/", "/webjars/**", "/login", "/resources/**").permitAll()
+                            .antMatchers("/beers/find", "/beers*").permitAll()
+                            .antMatchers(HttpMethod.GET, "/api/v1/beer/**").permitAll()
+                            .mvcMatchers(HttpMethod.GET, "/api/v1/beerUpc/{upc}").permitAll();
+                } )
                 .authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
